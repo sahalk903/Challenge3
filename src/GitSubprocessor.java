718ac41e333dc4b609388c34d9dab8e727fce83f
sahalk903 <54646495+sahalk903@users.noreply.github.com>
2021-04-12 11:00:12 -0400

    git subprocessor methods

--- src/GitSubprocessor.java
+++ src/GitSubprocessor.java
@@ -1,3 +1,28 @@
 
 public class GitSubprocessClient {
-}
+    GitSubprocessClient gitSubprocessClient = new GitSubprocessClient(repoPath);
+    String gitInit = gitSubprocessClient.getInit();
+    String gitRemoteAdd = gitSubprocessClient.getRemoteAdd("origin", "https://github.com/CSC109/GitSubprocessClient.git");
+    String gitAddFile = gitSubprocessClient.gitAddFile("GitSubprocessClient.java");
+    String gitAddAll = gitSubprocessClient.gitAddAll();
+    String commitMessage = "This is a commit message";
+    String commit = gitSubprocessClient.gitCommit(commitMessage);
+    String push = gitSubprocessClient.gitPush("master");
+    String status = gitSubprocessClient.gitStatus();
+    String log = gitSubprocessClient.gitLog(5);
+    String log = gitSubprocessClient.gitLogOneLine(5);
+    String log = gitSubprocessClient.gitLogAll();
+    String log = gitSubprocessClient.gitLogAllOneLine();
+    String checkout = gitSubprocessClient.switchBranch("master");
+    String createBranch = gitSubprocessClient.createBranch("new-branch");
+    String createBranch = gitSubprocessClient.createAndSwitchBranch("new-branch");
+    String deleteBranch = gitSubprocessClient.deleteBranch("new-branch");
+    String branches = gitSubprocessClient.listBranches();
+    String pull = gitSubprocessClient.gitPull("master");
+    String fetch = gitSubprocessClient.gitFetch("master");
+    String fetch = gitSubprocessClient.gitFetchAll();
+    String stash = gitSubprocessClient.gitStash();
+    String stashApply = gitSubprocessClient.gitStashApply();
+    String clone = gitSubprocessClient.gitClone();
+    String commit = gitSubprocessClient.runGitCommand("push origin master");
+    }
