26b5e2186fd9585799d7ae805cdf5103cf92b0a7
sahalk903 <54646495+sahalk903@users.noreply.github.com>
2021-04-14 22:34:45 -0400

    fixed all issues besides the RepoName JText Field and git add on it

--- src/GitSubprocessor.java
+++ src/GitSubprocessor.java
@@ -1,39 +1,43 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import git.tools.client.GitSubprocessClient;
 
-public class GitSubprocessClient {
-    GitSubprocessClient gitSubprocessClient = new GitSubprocessClient(repoPath);
-
-
-    public class Button extends JFrame {
+public class GitSub extends JFrame {
      protected JTextField commitMessage;
      private JButton button;
+     private TokenProcessor g;
 
+     GitSub() {
+         g = new TokenProcessor();
 
-     // JTextField commit message
+         // JTextField commit message
 
         this.add(new JLabel("Commit Message"));
         commitMessage = new JTextField();
         commitMessage.setColumns(30);
         this.add(commitMessage);
 
+         this.add(new JLabel("Repo Name"));
+         commitMessage = new JTextField();
+         commitMessage.setColumns(30);
+         this.add(repoPath);
+
         // Button to call add, commit, push
 
         button = new JButton("button");
-        enter.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed (ActionEvent e){
-             gitSubprocessClient.gitPush("master");
-             gitSubprocessClient.gitAddAll();
-             gitSubprocessClient.gitCommit(commitMessage);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                GitSubprocessClient gitSubprocessClient = new GitSubprocessClient(repoPath);
+                gitSubprocessClient.gitPush("master");
+                gitSubprocessClient.getText();
 
-         }
-     }
 
-         this.add(enter);
+            }
+        }
+
+         this.add(button);
          this.setVisible(true);
 
     }
